16009abba9cfb89636879c88a630f905ffaa5d16
ViniciusSouza1988 <viniciusdesouzasilveira1988@gmail.com>
2023-03-25 21:49:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+Console.WriteLine("Primeiro Número:");
+int v1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Segundo Número:");
+int v2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Terceiro Número:");
+int v3 = int.Parse(Console.ReadLine());
+
+int Maior = v1;
+
+if (v2 > Maior)
+{
+    Maior = v2;
+}
+if (v3 > Maior)
+{
+    Maior = v3;
+}
+
+Console.WriteLine("O maior número é:" + Maior);
+Console.WriteLine("Programa Finalizado");
+
+   
+
+
